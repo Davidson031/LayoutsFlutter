ef2976f1254d4e0a13062cff882faa9d571ffba0
Davidson <davidson@hotmail.com>
2022-10-29 21:14:25 -0300

    Primeiro layout concluido

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,120 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //--------------COMPONENTE TITULO------------------
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(30),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('40'),
+        ],
+      ),
+    );
+    //-------------------------------------------------
+
+    //--------------COMPONENTE BOTOES------------------
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        // mainAxisSize: MainAxisSize.min,
+        // mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(
+            icon,
+            color: color,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(Colors.grey, Icons.call, 'CALL'),
+        _buildButtonColumn(Colors.grey, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(Colors.grey, Icons.share, 'SHARE'),
+      ],
+    );
+    //-------------------------------------------------
+
+    //-------------COMPONENTE TEXTO--------------------
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    //-------------------------------------------------
+
+    Image imagem = Image.asset('images/lake.jpg',
+        width: 600, height: 240, fit: BoxFit.cover);
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+      home: Scaffold(
+          body: SingleChildScrollView(
+        child: Column(
+          children: [
+            imagem,
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        ),
+      )),
     );
   }
 }
